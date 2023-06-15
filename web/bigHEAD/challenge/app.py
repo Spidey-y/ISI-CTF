3384930e942cb7c8bc1b22460a62385779bdfd58
Spidey-y <hammoucheadel0@gmail.com>
2023-06-15 23:36:21 +0000

    fixed web chall

--- web/bigHEAD/challenge/app.py
+++ web/bigHEAD/challenge/app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     if request.method == 'HEAD':
-        if request.headers.get('User-Agent') == 'BigHe3dBrowser':
+        if request.headers.get('User-Agent') == 'BigH34dBrowser':
             return 'ISICTF{y4y_1_kn0w_H77P_h34d3r5}'
         else:
-            return 'Sorry, only users who use BigHe3dBrowser are allowed :('
+            return 'Sorry, only users who use BigH34dBrowser are allowed :('
     else:
         return 'Your HEAD isn\'t big enough :P'
 
